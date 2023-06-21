d5f7b81c4d31f5690b4b6cdd3b9fb5b1cfd8e534
AdzLovelace <heyinyu.sensi@foxmail.com>
2023-06-21 17:47:28 +0800

    增加了照明接口

--- device/lighting/apps.py
+++ device/lighting/apps.py
@@ -20,10 +20,21 @@ class LightingProtocol(Protocol):
         reactor.callInThread(self.query_light_brightness)
 
     def query_light_brightness(self):
+        modbus_header = "\x00\x00\x00\x00\x00\x06"
+        device_address = "\x01"
+        func_code ="\x03"
+        # 受报文返回长度限制，每次查询72个寄存器
+        register_address_list = ["\x05\x79","\x05\xC1"]
+        read_num = "\x00\x48"
+        msg1 = ''.join([modbus_header, device_address,
+                        func_code, register_address_list[0], read_num])
+        msg2 = ''.join([modbus_header, device_address,
+                        func_code, register_address_list[1], read_num])
+        msg = ''.join([msg1, msg2])
+
         while True:
-            self.transport.write(b"\x00\x00\x00\x00\x00\x06\x01\x03\x05\x79\x00\x48")
-            self.transport.write(b"\x00\x00\x00\x00\x00\x06\x01\x03\x05\xC1\x00\x48")
-            time.sleep(2)
+            self.transport.write(msg.encode())
+            time.sleep(5)
     def dataReceived(self, data: bytes):
 
 
@@ -32,7 +43,7 @@ class LightingProtocol(Protocol):
 
     # 解析函数，提取有用参数
     def parse_data(self, data: bytes):
-        print(data.hex())
+        pass
 
     def connectionLost(self, reason: failure.Failure = connectionDone):
         print("Disconnected from the server!")
@@ -58,9 +69,3 @@ class LightingClientFactory(ReconnectingClientFactory):
         print('Connection failed. Reason:', reason)
         ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
 
-
-lt = LightingClientFactory()
-#reactor.connectTCP("172.16.11.19",1001, lt)
-reactor.connectTCP("127.0.0.1",1001, lt)
-
-reactor.run()
